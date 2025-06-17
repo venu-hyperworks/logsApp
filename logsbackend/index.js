f01d3fdf173dcb5dde231267efcf1011b88ad97c
SREEKAR2499 <vsssreekar2499@gmail.com>
2025-06-17 20:17:56 +0530

    changes

--- logsbackend/index.js
+++ logsbackend/index.js
@@ -38,11 +38,11 @@ app.get('/api/read-log', async (req, res) => {
 
 // ✅ Start server
 const PORT = process.env.PORT || 3001;
-if (process.env.NODE_ENV === "production") {
+//if (process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "../logsfrontend/build")));
   
     app.get("*", (req, res) => {
       res.sendFile(path.join(__dirname, "../logsfrontend", "build", "index.html"));
     });
-  }
+ // }
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
