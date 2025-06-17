4a0394341e0880932d7b2a926b197c381adb3288
SREEKAR2499 <vsssreekar2499@gmail.com>
2025-06-17 20:57:06 +0530

    changes 4

--- logsbackend/index.js
+++ logsbackend/index.js
@@ -43,8 +43,8 @@ const PORT = process.env.PORT || 3001;
 //if (process.env.NODE_ENV === "production") {
     //app.use(express.static(path.join(__dirname, "../logsfrontend/build")));
   
-   // app.get("*", (req, res) => {
-      //res.sendFile(path.join(__dirname, "../logsfrontend", "build", "index.html"));
-   // });
+   //app.get("*", (req, res) => {
+     // res.sendFile(path.join(__dirname, "../logsfrontend", "build", "index.html"));
+   //});
  // }
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
